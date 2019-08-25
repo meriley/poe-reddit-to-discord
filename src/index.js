cf7a26a6d50c7162ba365902878068ab617fd690
merileyjr <merileyjr@gmail.com>
2019-08-24 22:32:32 -0700

    modified config

--- src/index.js
+++ src/index.js
@@ -2,74 +2,76 @@ const Snooper = require("reddit-snooper");
 const hookcord = require("hookcord");
 const config = require("./config.json");
 
-const app_id = process.env.POE_TO_DISCORD_ID;
-const api_secret = process.env.POE_TO_DISCORD_SECRET;
-const subreddit = "pathofexile";
 const REDDIT_URL = "https://www.reddit.com";
 
-const { author, webhooks } = config;
+const {
+  credentials: { clientId: app_id, clientSecret: api_secret },
+  data: { subreddits }
+} = config;
 const snooper = new Snooper({
   app_id,
   api_secret,
 
   automatic_retries: true,
-  api_requests_per_minuite: 60
+  api_requests_per_minuite: 30
 });
 
-console.log(
-  "Watching the subreddit ",
-  subreddit,
-  "with the applicaiton Id:",
-  app_id,
-  "secret:",
-  api_secret
-);
+subreddits.forEach(({ subreddit, monitor: { authors, webhooks } }) => {
+  console.log(
+    "Watching the subreddit",
+    subreddit,
+    "with the applicaiton Id:",
+    app_id,
+    "secret:",
+    api_secret
+  );
 
-snooper.watcher
-  .getPostWatcher(subreddit)
-  .on("post", function(post) {
-    if (
-      authors
-        .map(author => author.toLowerCase())
-        .includes(post.data.author.toLowerCase())
-    ) {
-      console.log("Post was posted by: " + post.data.author);
-      const {
-        data: { author, title, url, permalink, thumbnail }
-      } = post;
+  snooper.watcher
+    .getPostWatcher(subreddit)
+    .on("post", function(post) {
+      if (
+        authors
+          .map(author => author.toLowerCase())
+          .includes(post.data.author.toLowerCase())
+      ) {
+        console.log("Post was posted by: " + post.data.author);
+        const {
+          data: { author, title, url, permalink, thumbnail }
+        } = post;
 
-      const discordMessage = {
-        content: `${REDDIT_URL}${permalink}`,
-        embeds: [
-          {
-            author: {
-              name: author
-            },
-            title,
-            url: `${REDDIT_URL}${permalink}`,
-            thumbnail: validURL(thumbnail)
-              ? {
-                  url: thumbnail
-                }
-              : undefined,
-            fields: [{ name: "Content:", value: url }]
-          }
-        ]
-      };
+        const discordMessage = {
+          content: `${REDDIT_URL}${permalink}`,
+          embeds: [
+            {
+              author: {
+                name: author
+              },
+              title,
+              url: `${REDDIT_URL}${permalink}`,
+              thumbnail: validURL(thumbnail)
+                ? {
+                    url: thumbnail
+                  }
+                : undefined,
+              fields: [{ name: "Content:", value: url }]
+            }
+          ]
+        };
 
-      webhooks.forEach(url =>
-        new hookcord.Hook()
-          .setLink(url.url)
-          .setPayload(discordMessage)
-          .fire()
-          .then(function(response) {
-            console.log("response", response);
-          })
-          .catch(function(e) {})
-      );
-    }
-  })
-  .on("error", console.error);
+        webhooks.forEach(webhook =>
+          new hookcord.Hook()
+            .setLink(webhook.url)
+            .setPayload(discordMessage)
+            .fire()
+            .then(function(response) {
+              console.log("response", response);
+            })
+            .catch(function(e) {})
+        );
+      }
+    })
+    .on("error", console.error);
+});
 
 function validURL(str) {
   var pattern = new RegExp(
