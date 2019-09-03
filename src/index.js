ec7bae951947a2ca7de9e32b09919687f949a83e
merileyjr <merileyjr@gmail.com>
2019-09-02 19:25:04 -0700

    made more search paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Snooper = require("reddit-snooper");
 const hookcord = require("hookcord");
 const config = require("./config.json");
+const { get, map } = require("lodash");
 
 const REDDIT_URL = "https://www.reddit.com";
 
@@ -13,10 +14,10 @@ const snooper = new Snooper({
   api_secret,
 
   automatic_retries: true,
-  api_requests_per_minuite: 30
+  api_requests_per_minuite: 5
 });
 
-subreddits.forEach(({ subreddit, monitor: { authors, webhooks } }) => {
+subreddits.forEach(({ subreddit, monitor: { filters, webhooks } }) => {
   console.log(
     "Watching the subreddit",
     subreddit,
@@ -29,49 +30,74 @@ subreddits.forEach(({ subreddit, monitor: { authors, webhooks } }) => {
   snooper.watcher
     .getPostWatcher(subreddit)
     .on("post", function(post) {
-      if (
-        authors
-          .map(author => author.toLowerCase())
-          .includes(post.data.author.toLowerCase())
-      ) {
-        console.log("Post was posted by: " + post.data.author);
-        const {
-          data: { author, title, url, permalink, thumbnail }
-        } = post;
+      const {
+        data: { author, title, url, permalink, thumbnail }
+      } = post;
+      const hasAuthorMatch = filter(
+        get(filters, "author.exact"),
+        get(filters, "author.regex"),
+        author.toLowerCase()
+      );
+      const hasTitleMatch = filter(
+        get(filters, "title.exact"),
+        get(filters, "title.regex"),
+        title.toLowerCase()
+      );
 
-        const discordMessage = {
-          content: `${REDDIT_URL}${permalink}`,
-          embeds: [
-            {
-              author: {
-                name: author
-              },
-              title,
-              url: `${REDDIT_URL}${permalink}`,
-              thumbnail: validURL(thumbnail)
-                ? {
-                    url: thumbnail
-                  }
-                : undefined,
-              fields: [{ name: "Content:", value: url }]
-            }
-          ]
-        };
+      const discordMessage = {
+        content: `${REDDIT_URL}${permalink}`,
+        embeds: [
+          {
+            author: {
+              name: author
+            },
+            title,
+            url: `${REDDIT_URL}${permalink}`,
+            thumbnail: validURL(thumbnail)
+              ? {
+                  url: thumbnail
+                }
+              : undefined,
+            fields: [{ name: "Content:", value: url }]
+          }
+        ]
+      };
+      console.log(discordMessage);
 
-        webhooks.forEach(webhook =>
-          new hookcord.Hook()
-            .setLink(webhook.url)
-            .setPayload(discordMessage)
-            .fire()
-            .then(function(response) {
-              console.log("response valid!");
-            })
-            .catch(function(e) {})
-        );
+      if (hasAuthorMatch || hasTitleMatch) {
+        notify(webhooks, discordMessage);
       }
     })
     .on("error", console.error);
 });
+
+function filter(exact, regex, target) {
+  console.log(exact, regex, target);
+  const exactMatch = map(
+    exact,
+    item => console.log(item.toLowerCase(), target) || item.toLowerCase()
+  ).includes(target);
+  const regexMatch = map(regex, rx => new RegExp(rx, "i")).reduce(
+    (acc, rx) =>
+      console.log(rx.test(target), target, rx) || acc || rx.test(target),
+    false
+  );
+
+  return exactMatch || regexMatch;
+}
+
+function notify(webhooks, discordMessage) {
+  webhooks.forEach(webhook =>
+    new hookcord.Hook()
+      .setLink(webhook.url)
+      .setPayload(discordMessage)
+      .fire()
+      .then(function(response) {
+        console.log("response valid!");
+      })
+      .catch(function(e) {})
+  );
+}
 
 function validURL(str) {
   var pattern = new RegExp(
